0f1886f949033b348d0f77dc254f39ef70090065
Daiyun199 <yenphung3000@gmail.com>
2024-06-06 16:10:11 +0700

    update,delete wpf order

--- JewelrySalesStoreData/Models/Net1702_221_4_JewelrySalesStoreContext.cs
+++ JewelrySalesStoreData/Models/Net1702_221_4_JewelrySalesStoreContext.cs
@@ -20,6 +20,8 @@ public partial class Net1702_221_4_JewelrySalesStoreContext : DbContext
     {
         optionsBuilder.UseSqlServer("data source=JUNNIE-LAPTOP;initial catalog=Net1702_221_4_JewelrySalesStore;user id=sa;password=12345;Integrated Security=True;TrustServerCertificate=True");
         base.OnConfiguring(optionsBuilder);
+        //optionsBuilder.UseSqlServer("data source=localhost;initial catalog=Net1702_221_4_JewelrySalesStore;user id=sa;password=12345;Integrated Security=True;TrustServerCertificate=True");
+        //base.OnConfiguring(optionsBuilder);
     }
 
     public virtual DbSet<Category> Categories { get; set; }
--- JewelrySalesStoreWPFApp/UI/wOrder.xaml.cs
+++ JewelrySalesStoreWPFApp/UI/wOrder.xaml.cs
@@ -29,19 +29,110 @@ namespace JewelrySalesStoreWPFApp.UI
             LoadGridOrders();
         }
 
-        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        private async void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                //int idTmp = -1;
+                //int.TryParse(txtCurrencyCode.Text, out idTmp);
 
+                var item = await _business.GetById(Guid.Parse(txtOrderId.Text));
+
+                if (item.Data == null)
+                {
+                    var currency = new Order()
+                    {
+                        OrderId = Guid.NewGuid(),
+                        PaymentMethod = txtPaymentMethod.Text,
+                        CompanyId = (Guid.Parse(txtComapanyId.Text)),
+                        CustomerId = (Guid.Parse(txtCustomerId.Text)),
+                        Date = DateTime.Now,
+                        Status = bool.Parse(txtStatus.Text),
+                        TotalPrice = double.Parse(txtTotalPrice.Text),
+                    };
+
+                    var result = await _business.Save(currency);
+                    MessageBox.Show(result.Message, "Save");
+                }
+                else
+                {
+                    var currency = item.Data as Order;
+                    //currency.CurrencyCode = txtCurrencyCode.Text;
+                    currency.CompanyId = Guid.Parse(txtComapanyId.Text);
+                    currency.Status = bool.Parse(txtStatus.Text);
+                    currency.TotalPrice = double.Parse(txtTotalPrice.Text);
+                    currency.CompanyId = Guid.Parse(txtComapanyId.Text);
+                    currency.PaymentMethod = txtPaymentMethod.Text;
+                    currency.Date = DateTime.Now;
+                    var result = await _business.Update(currency);
+                    MessageBox.Show(result.Message, "Update");
+                }
+
+                txtComapanyId.Text = string.Empty;
+                txtStatus.Text = string.Empty;
+                txtTotalPrice.Text = string.Empty;
+                txtCustomerId.Text = string.Empty;
+                txtStatus.Text = string.Empty;
+                txtOrderId.Text = string.Empty;
+
+                this.LoadGridOrders();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
         }
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        private async void grdOrder_ButtonDelete_Click(object sender, RoutedEventArgs e) { }
+        private async void grdOrder_ButtonDelete_Click(object sender, RoutedEventArgs e) {
+            Button btn = (Button)sender;
+
+            string currencyCode = btn.CommandParameter.ToString();
+
+            //MessageBox.Show(currencyCode);
+
+            if (!string.IsNullOrEmpty(currencyCode))
+            {
+                if (MessageBox.Show("Do you want to delete this item?", "Delete", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    var result = await _business.DeleteById(Guid.Parse(currencyCode));
+                    MessageBox.Show($"{result.Message}", "Delete");
+                    this.LoadGridOrders();
+                }
+            }
+        }
         private async void grdOrder_ButtonEdit_Click(object sender, RoutedEventArgs e) { }
 
-        private async void grdOrder_MouseDouble_Click(object sender, RoutedEventArgs e) { }
+        private async void grdOrder_MouseDouble_Click(object sender, RoutedEventArgs e) {
+            DataGrid grd = sender as DataGrid;
+            if (grd != null && grd.SelectedItems != null && grd.SelectedItems.Count == 1)
+            {
+                var row = grd.ItemContainerGenerator.ContainerFromItem(grd.SelectedItem) as DataGridRow;
+                if (row != null)
+                {
+                    var item = row.Item as Order;
+                    if (item != null)
+                    {
+                        var currencyResult = await _business.GetById(item.OrderId);
+
+                        if (currencyResult.Data != null)
+                        {
+                            item = currencyResult.Data as Order;
+
+                            txtComapanyId.Text = item.CompanyId.ToString();
+                            txtCustomerId.Text = item.CustomerId.ToString();
+                            txtPaymentMethod.Text = item.PaymentMethod;
+                            txtStatus.Text = item.Status.ToString();
+                            txtTotalPrice.Text = item.TotalPrice.ToString();
+                            txtOrderId.Text = item.OrderId.ToString();
+                        }
+                    }
+                }
+            }
+        }
 
 
         private async void LoadGridOrders()
