d24d58ef82fcb843b3f0fb8f8ea7c21736c8c20c
Nguyễn Dũng <127070279+milod172@users.noreply.github.com>
2024-06-15 14:10:04 +0700

    CustomerPage

---
+++ JewelrySalesStoreRazorWebApp/Pages/CustomerPage/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using JewelrySalesStoreData.Models;
+using JewelrySalesStoreBusiness;
+
+namespace JewelrySalesStoreRazorWebApp.Pages.CustomerPage
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ICustomerBusiness business;
+        public CreateModel()
+        {
+            business ??= new CustomerBusiness();
+        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await business.Save(Customer);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ JewelrySalesStoreRazorWebApp/Pages/CustomerPage/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using JewelrySalesStoreData.Models;
+using JewelrySalesStoreBusiness;
+
+namespace JewelrySalesStoreRazorWebApp.Pages.CustomerPage
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ICustomerBusiness business;
+
+        public DetailsModel()
+        {
+            business ??= new CustomerBusiness();
+        }
+
+        public Customer Customer { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await business.GetById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            Customer = customer.Data as Customer;
+            return Page();
+        }
+    }
+}
