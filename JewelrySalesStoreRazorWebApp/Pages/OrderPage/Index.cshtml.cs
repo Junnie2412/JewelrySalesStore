406950cbde9cee81ef5474ca1792cf6d58e9fb75
Daiyun199 <yenphung3000@gmail.com>
2024-06-15 23:54:11 +0700

    search

--- JewelrySalesStoreRazorWebApp/Pages/OrderPage/Index.cshtml.cs
+++ JewelrySalesStoreRazorWebApp/Pages/OrderPage/Index.cshtml.cs
@@ -7,30 +7,37 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using JewelrySalesStoreData.Models;
 using JewelrySalesStoreBusiness.BusinessOrder;
+using JewelrySalesStoreBusiness;
 
 namespace JewelrySalesStoreRazorWebApp.Pages.OrderPage
 {
     public class IndexModel : PageModel
     {
         //private readonly JewelrySalesStoreData.Models.Net1702_221_4_JewelrySalesStoreContext _context;
-        private readonly IOrderBusiness business;
+        private readonly IOrderBusiness _business;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
 
         public IndexModel()
         {
-            //_context = context;
-            business ??= new OrderBusiness();
+            _business ??= new OrderBusiness();
         }
 
-        public IList<Order> Order { get;set; } = default!;
+        public IList<Order> Order { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            var result = await business.GetAll();
-                //.Include(o => o.Company)
-                //.Include(o => o.Customer).ToListAsync();
-                if(result!=null && result.Status > 0 && result.Data!=null)
+            var resultl = await _business.GetAll();
+            if (resultl != null && resultl.Status > 0 && resultl.Data != null)
             {
-                Order = result.Data as List<Order>;
+                var newOrders = resultl.Data as List<Order>;
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                    newOrders = newOrders.Where(c => c.CustomerAddress != null && c.CustomerAddress.Contains(SearchString, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+
+                Order = newOrders;
             }
         }
     }
