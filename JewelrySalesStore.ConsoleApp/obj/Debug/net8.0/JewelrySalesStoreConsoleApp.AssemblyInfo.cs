30cd1594b7e899587c15828197c9d9bc584641ab
psp-xuanquy <you@example.comxquy118@gmail.com>
2024-06-20 21:50:30 +0700

    fix appsettings

--- JewelrySalesStore.ConsoleApp/obj/Debug/net8.0/JewelrySalesStoreConsoleApp.AssemblyInfo.cs
+++ JewelrySalesStore.ConsoleApp/obj/Debug/net8.0/JewelrySalesStoreConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JewelrySalesStoreConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d356fa6a7cd04a45a1523774c9360afbdf56229d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5a789c2a854c07205483cd1cef520f23fc9ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JewelrySalesStoreConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JewelrySalesStoreConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
