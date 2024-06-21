d3eeb9861fcc212ec2c04f137a96166a20d6ad64
Nguyễn Dũng <127070279+milod172@users.noreply.github.com>
2024-06-21 21:18:11 +0700

    Login Hidden Icon

--- JewelrySalesStoreRazorWebApp/Pages/Index.cshtml.cs
+++ JewelrySalesStoreRazorWebApp/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace JewelrySalesStoreRazorWebApp.Pages
@@ -13,36 +14,57 @@ namespace JewelrySalesStoreRazorWebApp.Pages
         [BindProperty]
         public string Password { get; set; }
 
-        public bool ShowNavBar { get; set; } = false;
+     
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet(bool? showNavBar)
+        public void OnGet()
         {
-           
-            ViewData["ShowNavBar"] = showNavBar ?? false; 
+            // Luôn hiển thị banner và footer
             ViewData["ShowBanner"] = true;
             ViewData["ShowFooter"] = true;
+
+            // Kiểm tra và thiết lập giá trị mặc định cho ShowNavBar
+            int? showNavBar = HttpContext.Session.GetInt32("ShowNavBar");
+            if (showNavBar == null)
+            {
+                HttpContext.Session.SetInt32("ShowNavBar", 0); // 0 = false
+                ViewData["ShowNavBar"] = false;
+            }
+            else
+            {
+                ViewData["ShowNavBar"] = showNavBar == 1;
+            }
+
+            // Kiểm tra và thiết lập giá trị mặc định cho ShowLogin
+            int? showLogin = HttpContext.Session.GetInt32("ShowLogin");
+            if (showLogin == null)
+            {
+                HttpContext.Session.SetInt32("ShowLogin", 1); // 1 = true
+                ViewData["ShowLogin"] = true;
+            }
+            else
+            {
+                ViewData["ShowLogin"] = showLogin == 1;
+            }
         }
 
         public IActionResult OnPostLogin(bool? showNavBar)
         {
             if (Username == "admin" && Password == "123")
             {
-                ShowNavBar = true;
+                HttpContext.Session.SetInt32("ShowNavBar", 1); // 1 = true
+                HttpContext.Session.SetInt32("ShowLogin", 0); // 0 = false
             }
             else
             {
-                ShowNavBar = false;
+                HttpContext.Session.SetInt32("ShowNavBar", 0); // 0 = false
+                HttpContext.Session.SetInt32("ShowLogin", 1); // 1 = true
             }
 
-            ViewData["ShowBanner"] = true;
-            ViewData["ShowFooter"] = true;
-            ViewData["ShowNavBar"] = ShowNavBar;
-
-            return Page();
+            return RedirectToPage("/Index");
         }
     }
 }
