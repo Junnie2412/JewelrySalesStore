e81940549bc7bcab65b0bbd6ac11508a559f7f7d
Junnie2412 <dungchan321@gmail.com>
2024-06-21 13:34:48 +0700

    SearchCategory

--- JewelrySalesStore.ConsoleApp/obj/Debug/net8.0/JewelrySalesStoreConsoleApp.AssemblyInfo.cs
+++ JewelrySalesStore.ConsoleApp/obj/Debug/net8.0/JewelrySalesStoreConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JewelrySalesStoreConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd5a789c2a854c07205483cd1cef520f23fc9ed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbcc84b0e1aaea12403f0c38cf95377008c15d62")]
 [assembly: System.Reflection.AssemblyProductAttribute("JewelrySalesStoreConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JewelrySalesStoreConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- JewelrySalesStoreRazorWebApp/Pages/CategoryPage/Index.cshtml.cs
+++ JewelrySalesStoreRazorWebApp/Pages/CategoryPage/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using JewelrySalesStoreData.Models;
 using JewelrySalesStoreBusiness;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace JewelrySalesStoreRazorWebApp.Pages.CategoryPage
 {
@@ -14,8 +15,16 @@ namespace JewelrySalesStoreRazorWebApp.Pages.CategoryPage
     {
         private readonly ICategoryBusiness _business;
 
-        [BindProperty(SupportsGet = true)]  
+        [BindProperty(SupportsGet = true)]
         public string? SearchName { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchPurpose { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public bool IsActive { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public bool IsNotActive { get; set; }
         [BindProperty(SupportsGet = true)]
         public int PageIndex { get; set; } = 1;
         public int TotalPages { get; set; }
@@ -27,16 +36,40 @@ namespace JewelrySalesStoreRazorWebApp.Pages.CategoryPage
         }
 
         public IList<Category> Category { get; set; } = default!;
+        public List<SelectListItem> PurposeOptions { get; set; } = new List<SelectListItem>();
 
         public async Task OnGetAsync()
         {
             var resultl = await _business.GetAll();
+
             if (resultl != null && resultl.Status > 0 && resultl.Data != null)
             {
                 var newCategories = resultl.Data as List<Category>;
+
+                PurposeOptions = newCategories
+                .Select(c => c.Purpose)
+                .Distinct()
+                .Where(p => !string.IsNullOrEmpty(p))
+                .Select(p => new SelectListItem { Value = p, Text = p })
+                .ToList();
+
                 if (!string.IsNullOrEmpty(SearchName))
                 {
                     newCategories = newCategories.Where(c => c.Name != null && c.Name.Contains(SearchName, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+                if (!string.IsNullOrEmpty(SearchPurpose))
+                {
+                    newCategories = newCategories.Where(c => c.Purpose != null && c.Purpose.Contains(SearchPurpose)).ToList();
+                }
+
+                if (IsActive)
+                {
+                    newCategories = newCategories.Where(c => (bool)c.IsActive).ToList();
+                }
+
+                if (IsNotActive)
+                {
+                    newCategories = newCategories.Where(c => (bool)!c.IsActive).ToList();
                 }
 
                 Category = newCategories;
