48bee90e1435d81b6139f1a73831dabf6bb1c3a8
Phan Minh Tùng <110610398+tungphan1011@users.noreply.github.com>
2024-06-23 01:03:32 +0700

    tung

---
+++ JewelrySalesStoreData/DateRangeAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelrySalesStoreData
+{
+    public class DateRangeAttribute : ValidationAttribute
+    {
+        private readonly string _startDatePropertyName;
+
+        public DateRangeAttribute(string startDatePropertyName)
+        {
+            _startDatePropertyName = startDatePropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var endDate = (DateTime?)value;
+
+            var startDateProperty = validationContext.ObjectType.GetProperty(_startDatePropertyName);
+            if (startDateProperty == null)
+            {
+                return new ValidationResult($"Unknown property: {_startDatePropertyName}");
+            }
+
+            var startDate = (DateTime?)startDateProperty.GetValue(validationContext.ObjectInstance);
+
+            if (startDate.HasValue && endDate.HasValue && startDate.Value > endDate.Value)
+            {
+                return new ValidationResult("StartDate must be less than EndDate.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
