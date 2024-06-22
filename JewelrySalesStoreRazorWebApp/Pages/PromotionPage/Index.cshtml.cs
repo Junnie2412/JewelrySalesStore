8af907c00dcfb5c04afb3868e828068778fd10e3
Phan Minh Tùng <110610398+tungphan1011@users.noreply.github.com>
2024-06-22 22:41:27 +0700

    fix bug

--- JewelrySalesStoreRazorWebApp/Pages/PromotionPage/Index.cshtml.cs
+++ JewelrySalesStoreRazorWebApp/Pages/PromotionPage/Index.cshtml.cs
@@ -7,8 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using JewelrySalesStoreData.Models;
+
 using JewelrySalesStoreBusiness;
-using JewelrySalesStoreBusines;
 
 namespace JewelrySalesStoreRazorWebApp.Pages.PromotionPage
 {
